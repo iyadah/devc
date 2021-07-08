ab18027640c76c810f1067bd4ee55708e839fe52
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-07-08 09:19:13 +0300

    create profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
-// @route    GET api/profile
+const { check, validationResult } = require('express-validator');
+// @route    GET api/profile/me
 // @desc     Test route
 // @access   Public
-router.get('/me', auth, async (req,res) => {
+router.get('/me', auth, async (req, res) => {
     try{
         const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
 
@@ -20,5 +21,47 @@ router.get('/me', auth, async (req,res) => {
         res.status(500).send('Server error');
     }
 })
+
+
+// @route    POST api/profile
+// @desc     Create or update the user profile
+// @access   Private
+router.post('/', [auth, [
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skills is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ erros: errors.array() })
+    }
+       // destructure the request
+       const {
+        website,
+        skills,
+        youtube,
+        twitter,
+        instagram,
+        linkedin,
+        facebook,
+        // spread the rest of the fields we don't need to check
+      } = req.body;
+      // Build profile object
+      const profileFields = {};
+      profileFields.user = req.user.id;
+      if(company) profileFields.company = company;
+      if(website) profileFields.website = website;
+      if(location) profileFields.location = location;
+      if(bio) profileFields.bio = bio;
+      if(status) profileFields.status = status;
+      if(githubusername) profileFields.githubusername = githubusername;
+      if(skills){
+          profileFields.skills = skills.split(',').map(skill => skill.trim());
+
+      } 
+      console.log(skills);
+  
+      return res.send('^^1^^')
+
+});
 
 module.exports = router;
