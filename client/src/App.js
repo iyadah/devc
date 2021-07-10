5106c3cc506b007e0f8d3f4a4fdc9e248b0e5941
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-07-10 11:52:13 +0300

    Add client, Navbar, and Landing

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+import React, { Fragment } from 'react';
+
+const App = () => 
+  <Fragment>
+    <Navbar />
+    <Landing />
+
+  </Fragment>
+
+
+export default App;
