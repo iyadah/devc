9305ba30e85a0773ce2dd36fb10c3e7672603f86
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-07-14 17:16:03 +0400

    ureed2

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 export const Navbar = () => {
     return (
         <nav className="navbar bg-dark">
+       <img src={require('../../img/urood2.png')} style={{height: 60, width: 60,  display: 'block'}} />
         <h1>
           <Link to="/"><i className="fas fa-code"></i> Urood.com</Link>
         </h1>
