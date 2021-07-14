2c159265c7e501adc816e7fb8dbcf0339306ae9d
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-07-14 18:13:56 +0400

    images from cloudinary

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import logo from '../../img/urood.svg'
+import logo from '../../img/urood.svg'; //defined but not used now as we are calling from cloudinary
 
 export const Navbar = () => {
     return (
         <nav className="navbar bg-dark">
   {/*    <img src={require('../../img/urood2.png')} style={{height: 60, width: 60,  display: 'block'}} /> */} 
-        <img src={logo} style={{height: 60, width: 60,  display: 'block'}} />
+  {/*    <img src={logo} style={{height: 60, width: 60,  display: 'block'}} /> */} {/* This line to import the image from the local assets*/}
+        <img src='https://res.cloudinary.com/dkbror80w/image/upload/v1626271736/img/urood_uyqcxw.svg' style={{height: 60, width: 60,  display: 'block'}} />
         <h1>
           <Link to="/"><i className="fas fa-code"></i> Urood.com</Link>
         </h1>
