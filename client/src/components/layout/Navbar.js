8aefcec997b49a3835f933dbe44b51397615173c
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-07-14 17:47:51 +0400

    fix the logo position and style

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import logo from '../../img/urood.svg'
 
 export const Navbar = () => {
     return (
         <nav className="navbar bg-dark">
-       <img src={require('../../img/urood2.png')} style={{height: 60, width: 60,  display: 'block'}} />
+  {/*    <img src={require('../../img/urood2.png')} style={{height: 60, width: 60,  display: 'block'}} /> */} 
+        <img src={logo} style={{height: 60, width: 60,  display: 'block'}} />
         <h1>
           <Link to="/"><i className="fas fa-code"></i> Urood.com</Link>
         </h1>
