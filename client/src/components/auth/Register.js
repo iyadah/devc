f6e4da34ceccdb06d295e2ef5b11724842df3c7a
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-07-16 21:26:49 +0300

    clean comments

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -25,25 +25,6 @@ export const Register = ({ setAlert, register }) => {
             setAlert('Passwords do not match', 'danger', 500);
         } else{
             register({ name, email, password });
-        /*     const newUser = {
-        //         name, 
-        //         email, 
-        //         password
-        //     }
-        //     try {
-        //         const config = {
-        //             headers:{
-        //                 'Content-Type': 'application/json',
-        //             }
-        //         }
-        //         const body = JSON.stringify(newUser);
-        //         const res = await axios.post('/api/users', body, config);
-        //         console.log(res.data);
-                
-        //     } catch (err) {
-
-        //         console.error(err.response.data);
-        //    }*/
         }
     }
 
