7c7b953ea9b0ce6b50542498957d3a2e0eb12f25
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-07-24 17:18:11 +0300

    add profile to the state

--- client/src/components/dashboard/Dashobard.js
+++ client/src/components/dashboard/Dashobard.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
+import PrivateRoute from '../routing/PrivateRoute';
+import auth from '../../reducers/auth';
 
-const Dashobard = props => {
+const Dashobard = ({ getCurrentProfile, auth, profile }) => {
+    useEffect(() => {
+        getCurrentProfile();
+    }, []);
+
     return (
         <div>
             Dashboard
         </div>
     )
-}
+};
 
 Dashobard.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
 
-}
+};
 
-export default Dashobard;
+const mapStateToProps = state => ({
+    auth: state.auth,
+    profile: state.profile
+})
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashobard);
 
 
