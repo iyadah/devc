99a8ec0d5f1419270837be8c18c9eb061b87571f
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-07-27 13:37:52 +0300

    fix adding education issue

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -3,7 +3,29 @@ import PropTypes from 'prop-types';
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addEducation } from '../../actions/profile';
-function AddEducation(props) {
+function AddEducation({ addEducation, history}) {
+    const [formData, setFormData] = useState({
+        school: '',
+        degree: '',
+        fieldofstudy: '',
+        from: '',
+        to: '',
+        current: false,
+        description:''
+    });
+
+    const [toDateDisabled, toggleDisabled] = useState(false);
+
+    const {school='', degree='', fieldofstudy='', from='', to='', current, discription=''} = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = (e) => {
+        console.log(formData);
+        e.preventDefault();
+        addEducation(formData, history);
+      };
+
     return (
         <Fragment>
                   <h1 class="large text-primary">
@@ -14,12 +36,12 @@ function AddEducation(props) {
         you have attended
       </p>
       <small>* = required field</small>
-      <form class="form">
+      <form class="form" onSubmit={e => onSubmit(e)}>
         <div class="form-group">
           <input
             type="text"
             placeholder="* School or Bootcamp"
-            name="school"
+            name="school" value={school} onChange={e => onChange(e)}
             required
           />
         </div>
@@ -27,25 +49,28 @@ function AddEducation(props) {
           <input
             type="text"
             placeholder="* Degree or Certificate"
-            name="degree"
+            name="degree" value={degree} onChange={e => onChange(e)}
             required
           />
         </div>
         <div class="form-group">
-          <input type="text" placeholder="Field Of Study" name="fieldofstudy" />
+          <input type="text" placeholder="Field Of Study" name="fieldofstudy" value={fieldofstudy} onChange={e => onChange(e)} />
         </div>
         <div class="form-group">
           <h4>From Date</h4>
-          <input type="date" name="from" />
+          <input type="date" name="from" value={from} onChange={e => onChange(e)} />
         </div>
         <div class="form-group">
           <p>
-            <input type="checkbox" name="current" value="" /> Current School or Bootcamp
+            <input type="checkbox" name="current" value={current} checked={current} onChange={e => {
+              setFormData({ ...formData, current: !current });
+              toggleDisabled(!toDateDisabled);
+          }} /> {' '} Current School or Bootcamp
           </p>
         </div>
         <div class="form-group">
           <h4>To Date</h4>
-          <input type="date" name="to" />
+          <input type="date" name="to" value={to} onChange={e => onChange(e)} disabled={toDateDisabled ? 'disabled' : ''} />
         </div>
         <div class="form-group">
           <textarea
