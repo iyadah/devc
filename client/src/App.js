af4b48202ebd977238d8c2e1191ad385ab70af16
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-07-30 12:02:31 +0300

    get profile

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import PrivateRoute from './components/routing/PrivateRoute';
+import Profile from './components/profile/Profile';
 
 import { loadUser } from './actions/auth';
 
@@ -42,6 +43,7 @@ return(
        <Route exact path='/register' component={ Register } />
        <Route exact path='/login' component={ Login } />
        <Route exact path='/profiles' component={ Profiles } />
+       <Route exact path='/profile/:id' component={ Profile } />
 
        <PrivateRoute exact path='/dashboard' component={ Dashboard } />
        <PrivateRoute exact path='/create-profile' component={ CreateProfile } />
---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,32 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getProfileById, getProfiles } from '../../actions/profile';
+
+
+function Profile({ getProfileById, profile: {profile, loading}, match }) {
+    useEffect(()=>{
+        getProfileById(match.params.id);
+    }, [getProfileById]);
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Profile.propTypes = {
+    getProfileById: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired
+
+}
+
+const mapStateToProps = state => ({
+    profile: state.profile,
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, {getProfileById})(Profile)
+
