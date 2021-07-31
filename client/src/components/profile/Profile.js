3a98612d4d2c94f966bebfbe9aaba5dbbabc8863
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-07-31 08:25:05 +0300

    view/edit profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,32 +1,49 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
-import { getProfileById, getProfiles } from '../../actions/profile';
+import { getProfileById } from '../../actions/profile';
+
+const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
+  useEffect(() => {
+    getProfileById(match.params.id);
+  }, [getProfileById, match.params.id]);
+
+  return (
+    <Fragment>
+      {profile === null ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Link to="/profiles" className="btn btn-light">
+            Back To Profiles
+          </Link>
+          {auth.isAuthenticate &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to="/edit-profile" className="btn btn-dark">
+                Edit Profile
+              </Link>
+            )}
 
 
-function Profile({ getProfileById, profile: {profile, loading}, match }) {
-    useEffect(()=>{
-        getProfileById(match.params.id);
-    }, [getProfileById]);
-    return (
-        <div>
-            
-        </div>
-    )
-}
+</Fragment>
+)}
+</Fragment>
+);
+};
 
 Profile.propTypes = {
-    getProfileById: PropTypes.func.isRequired,
-    profile: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired
+getProfileById: PropTypes.func.isRequired,
+profile: PropTypes.object.isRequired,
+auth: PropTypes.object.isRequired
+};
 
-}
-
-const mapStateToProps = state => ({
-    profile: state.profile,
-    auth: state.auth
+const mapStateToProps = (state) => ({
+profile: state.profile,
+auth: state.auth
 });
 
-export default connect(mapStateToProps, {getProfileById})(Profile)
+export default connect(mapStateToProps, { getProfileById })(Profile);
 
