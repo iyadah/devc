15131dc6e73d5d366fdc6710fc61a518f9b61965
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-08-06 12:21:09 +0300

    add some react stuff to the exam

--- client/src/components/exams/ExamItem.js
+++ client/src/components/exams/ExamItem.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const examItem = ({exam: {title, description, numberOfQuestions, question}}) => {
     return (
-        <div className="profile bg-light">
-            <div>{title}</div>
-            <div>{description}</div>
-            <div>{numberOfQuestions}</div>
-            <div>{question.length}</div>
-
+        <div>
+        <div className="bg-light">
+            <div>Title: {title}</div>
+            <div>Description: {description}</div>
+            <div># of Questions: {numberOfQuestions}</div>
+            <div>Current questions #: {question.length}</div>
         </div>
+        <div>
+            <h2>Questions</h2>
+            {question.map(question=>(<h3>Question: {question.title}</h3>
+            )) }
+        <br />
+        <hr />
+        <br />
+       </div>
+
+       </div>
     )
 }
 
--- client/src/components/exams/Exams.js
+++ client/src/components/exams/Exams.js
@@ -8,7 +8,7 @@ import { getExams } from '../../actions/exam'
 const Exams = ({ getExams, exam: { exams, loading } }) => {
     useEffect(() => {
         getExams();
-    }, [getExams])
+    }, [getExams]);
     return (
         <div>
             <h1>Exam list</h1>
@@ -16,9 +16,11 @@ const Exams = ({ getExams, exam: { exams, loading } }) => {
                     {
                     exams.length > 0 ? (
                         exams.map(exam=>(<ExamItem key={exam._id} exam={exam} />
+                        
                         //exams.map(exam=>(<h1>{exam.description}</h1> 
                     ))): <h4> No Exams...</h4>
                 }
+
             </div>
 
         </div>
